a2ac808e895587f95a77f88f4fa88e97c87604bd
Soumya Zinzuwadia <iamsoumya13@gmail.com>
2021-06-15 16:49:24 +0530

    With Conditions !

--- football.py
+++ football.py
@@ -1,2 +1,8 @@
-print('I love Football !')
-print('Same Here DUD !');
+football = input('Do You love Football ?');
+if football == '1':
+    print('Awesome !');
+    print('Lets Play Together !');
+else:
+    cricket = input('Do you like Cricket ?');
+    if cricket == 'yes':
+        print('Lets Play Cricket !');
